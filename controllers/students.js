cd704bd574c2bb7cafa161fb52ac629eed085cfa
Subaru Goto <subaru.dayo@gmail.com>
2024-02-08 15:34:43 +0100

    update error handling

--- controllers/students.js
+++ controllers/students.js
@@ -4,13 +4,8 @@ import { tryAndCatch } from "../utils/tryAndCatch.js";
 export const addStudent = tryAndCatch(
   async (req, res, next) => {
     const { name, first_name, email } = req.body;
-    console.log(req.body)
     const data = await Student.create({ name, first_name, email });
-    if (data.length === 0) {
-      return next( {statusCode:400, message:"Failed to add a student data."} );
-    } else {
-      res.status(201).send(data);
-    }
+    res.status(201).send(data);
   }
 );
 
@@ -19,19 +14,15 @@ export const iNameYouBobJohn = tryAndCatch(
     const data = await Student.updateMany(
       { name:"john" }, { name:"bob" }, { new: true }
       );
-    if (data.length === 0) {
-      return next( {statusCode:400, message:"Failed to name you bob."} );
-    } else {
       res.status(200).send(data);
-    }
   }
 );
 
 export const getStudents = tryAndCatch(
   async (req, res, next) => {
     const data = await Student.find();
-    if (data.length === 0) {
-      return next( {statusCode:400, message:"Failed to get student data."} );
+    if (!data) {
+      return next( {statusCode:404, message:"No data found."} );
     } else {
       res.status(200).json(data);
     }
